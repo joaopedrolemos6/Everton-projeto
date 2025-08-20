5ad4fd499a14ff0f5a8d3e944edf9ca1469cfdcf
João Pedro De Araújo Lemos <144943620+joaopedrolemos6@users.noreply.github.com>
2025-08-20 15:24:36 -0300

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,8 +8,8 @@ const Team = () => {
     {
       name: 'Contador Everton Sousa',
       position: 'Sócio Fundador',
-      specialization: 'Contabilidade Empresarial e Tributária',
-      education: 'MBA FGV, ',
+      specialization: 'Especialista Contabilidade Empresarial e Tributaria Contabilidade para Área da Saúde
+      education: 'CRC: PB-013847/O-6 ',
       experience: '10 anos de experiência',
       image: profilePhoto 
     },
